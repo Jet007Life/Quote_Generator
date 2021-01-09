f2867f461630ed48df21f795dcbff0ee1477f7d7
Brian Ponder <bponder003@gmail.com>
2021-01-09 16:45:34 -0600

    script update

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 
 // Get Quote From API
 async function getQuote() {
+    loading();
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 
@@ -22,18 +38,33 @@ async function getQuote() {
         }
 
         // Reduce font size for long quotes & retrieve quote field from api
-        if (data.quoteText.length > 50) {
+        if (data.quoteText.length > 120) {
             quoteText.classList.add('long-quote');
         } else {
             quoteText.classList.remove('long-quote');
         }   
         quoteText.innerText = data.quoteText;     
         // ^^^ If string is too long use CSS class(long-quote) to shrink font, else remove the class just in case previous quote was not too long.
+        
+        // Stop Loader, Show Quote
+        complete()
 
     } catch (error) {
         getQuote();
         console.log('Hey!!! Fix this error', error);
     }
+
+    // Tweet Quote
+    function tweetQuote() {
+        const quote = quoteText.innerText;
+        const author = quoteText.innerText;
+        const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+        window.open(twitterUrl, '_blank');
+    }
+
+    // Event Listeners
+    newQuoteBtn.addEventListener('click', getQuote);
+    twitterBtn.addEventListener('click', tweetQuote); 
 }
 
 // On Load
